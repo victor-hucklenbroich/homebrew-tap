7e7c57531543641104bb193b07d7689d2aa6d0d4
Victor Hucklenbroich <victor-h@outlook.de>
2026-06-16 15:43:17 +0200

    Add DeskController cask

--- Casks/desk-controller.rb
+++ Casks/desk-controller.rb
@@ -1,1 +1,37 @@
+cask "desk-controller" do
+  version "[RELEASE_VERSION]"
+  sha256 "[RELEASE_SHA256]"
 
+  url "https://github.com/victor-hucklenbroich/desk-controller/releases/download/v#{version}/DeskController-#{version}.zip",
+      verified: "github.com/victor-hucklenbroich/desk-controller/"
+  name "DeskController"
+  desc "Menu bar app for Linak-based standing desks"
+  homepage "https://github.com/victor-hucklenbroich/desk-controller"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: :sonoma
+  # The app shells out to linak-controller at runtime
+  depends_on formula: "victor-hucklenbroich/tap/linak-controller"
+
+  app "DeskController.app"
+
+  zap trash: [
+    "~/Library/Application Support/linak-controller",
+    "~/Library/Logs/DeskController.log",
+    "~/Library/Logs/DeskController_error.log",
+  ]
+
+  caveats <<~EOS
+    DeskController controls a Linak based standing desk via Bluetooth. Before first use:
+
+      * Make sure your desk is already paired to this Mac via Bluetooth.
+      * Launch DeskController from /Applications and enter your desk's UUID.
+
+    The required 'linak-controller' CLI is installed automatically as a
+    dependency of this cask.
+  EOS
+end
